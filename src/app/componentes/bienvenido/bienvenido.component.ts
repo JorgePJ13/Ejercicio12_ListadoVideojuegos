35cefc4203c3c0e0a3b38a66aeefde46c15b300e
Jorge Pando Jimeno <jorge.pando@babelgroup.com>
2022-04-01 10:48:13 +0200

    Corregir un error

--- src/app/componentes/bienvenido/bienvenido.component.ts
+++ src/app/componentes/bienvenido/bienvenido.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, ResolvedReflectiveFactory } from "@angular/core";
 import { Router } from "@angular/router";
+import { Usuario } from "src/app/entidades/Usuario";
 import { Videojuego } from "src/app/entidades/videojuego";
 
 @Component({
@@ -13,11 +14,16 @@ export class BienvenidoComponent implements OnInit {
     videojuegos: Videojuego[] = []; 
     videojuego: Videojuego | null = null;
 
+    arrayUsuarios : Usuario[] = []; 
+
     id : number = 0;
     titulo : string = ""
     imagen : string = ""
     compania : string = ""
     valoracion : number = 0;
+
+    usuario : string =""
+    password : string = ""
 
     constructor(private router:Router) {
         let v : Videojuego = new Videojuego(1, "The Last Of Us", "./assets/TLOU.jpg");
@@ -26,6 +32,9 @@ export class BienvenidoComponent implements OnInit {
         this.videojuegos.push(v);
         v = new Videojuego(3, "Elden Ring", "./assets/ER.jpg", "From Software", 9.7)
         this.videojuegos.push(v)
+
+        let usuario : Usuario = new Usuario("JorgePJ13", "admin12345")
+        this.arrayUsuarios.push(usuario)
     }
 
     public routingProgramatico() {
